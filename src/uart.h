984dca589d86e050d1ec57ec75c1f2a52a4d6741
erik.juvan <erik.juvan@gorenje.com>
2023-11-13 23:09:13 +0100

    Remove uart.c,.h since its not needed

--- src/uart.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "esp_err.h"
-#include <stdint.h>
-
-int       uart_transmit(const char* data, int len);
-int       uart_receive(uint8_t* dest, int len);
-esp_err_t init_uart();
